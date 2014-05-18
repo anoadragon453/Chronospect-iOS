03767110e7f5608d41418f5abc909e53541283b4
Andrew Morgan <anoadragon453@gmail.com>
2014-05-18 13:36:17 -0700

    Initial Commit

---
+++ Chronospect/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Chronospect
+//
+//  Created by Andrew Morgan on 5/18/14.
+//  Copyright (c) 2014 Andrew Morgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Chronospect/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Chronospect
+//
+//  Created by Andrew Morgan on 5/18/14.
+//  Copyright (c) 2014 Andrew Morgan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
